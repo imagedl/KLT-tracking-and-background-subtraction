c12a740fe78a6bc9aecd3c5120573b141ef123d4
mohak <mohak.bhardwaj@gmail.com>
2015-10-20 00:02:22 -0400

    second-commit

--- code/testCarSequence.m
+++ code/testCarSequence.m
@@ -1,7 +1,8 @@
 
 %Load File
-carseq = load('../data/carseq.mat');
-rect(1,1:4)=[60,117,146,152];
+carseq = load('../data/sylvseq.mat');
+rect(1,1:4)=[102,62,156,108];
+
 figure(2)
 hold on
 for i=1:size(carseq.frames,3)-1
@@ -9,9 +10,12 @@ for i=1:size(carseq.frames,3)-1
 
 % rectangle('position', [rect(1), rect(2), rect(3)-rect(1), rect(4)-rect(2)], 'EdgeColor', 'g');
 It=carseq.frames(:,:,i);
+
+im = insertShape(It,'rectangle', [rect(i,1),rect(i,2), rect(i,3)-rect(i,1),rect(i,4)-rect(i,2)]);
+imshow(im);
 It1=carseq.frames(:,:,i+1);
 [u,v] = LucasKanade(It, It1, rect(i,1:4));
-rect(i+1,1:4)=rect(i,1:4) +[floor(u),floor(v),floor(u),floor(v)];
-%  imshow(It);
+rect(i+1,1:4)=  rect(i,1:4) +round([u,v,u,v]);
+ 
 end
 hold off
