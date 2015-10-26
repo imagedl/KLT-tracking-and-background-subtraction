656b0b8d8cd4174281ed5f181fcce6d0342f6f22
mohak <mohak.bhardwaj@gmail.com>
2015-10-26 16:14:35 -0400

    almost-complete

--- code/testCarSequence.m
+++ code/testCarSequence.m
@@ -13,7 +13,7 @@ for i=1:size(carseq.frames,3)-1
 % rectangle('position', [rect(1), rect(2), rect(3)-rect(1), rect(4)-rect(2)], 'EdgeColor', 'g');
 It=carseq.frames(:,:,i);
 
-im = insertShape(It,'rectangle', [rects(i,1),rects(i,2), rects(i,3)-rects(i,1),rects(i,4)-rects(i,2)], 'LineWidth', 3);
+im = insertShape(It,'rectangle', [rects(i,1),rects(i,2), rects(i,3)-rects(i,1),rects(i,4)-rects(i,2)], 'Color', 'green', 'LineWidth', 3);
 imshow(im);
 if(i==1 || i==4|| i==100|| i==125|| i==200 || i==300 || i==380||i==400)
 imnum= i;
